fa7a7ed9e14bbcc8a8f9542a9944a244ee4e2b2c
danieljimenez1337 <danieljimenez1337@gmail.com>
2021-08-29 15:00:33 -0700

    Created repository interface

---
+++ src/main/java/com/example/challenge/repository/VehicleRepository.java
@@ -0,0 +1,9 @@
+package com.example.challenge.repository;
+
+import com.example.challenge.model.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VehicleRepository extends JpaRepository<Vehicle, Integer> {
+}
